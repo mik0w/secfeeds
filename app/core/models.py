5326638c4afec7def282484580249348a2ab104a
kmpl <kmpl@protonmail.com>
2021-12-20 10:27:46 +0100

    unique id for url

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class FeedData(models.Model):
-    feed_url = models.CharField(max_length=350, primary_key=True)
+    feed_url = models.CharField(max_length=350, unique=True)
     feed_name = models.CharField(max_length=250, default='')
     is_verified = models.BooleanField(default=False)
 
